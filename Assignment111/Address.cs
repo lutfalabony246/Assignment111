9d68fc0789f3d1681117608209e38222c0a547b0
USER <18-36187-1@student.aiub.edu>
2021-03-02 00:50:56 +0600

    Address Setup

--- Assignment111/Address.cs
+++ Assignment111/Address.cs
@@ -8,5 +8,37 @@ namespace Assignment111
 {
 	class Address
 	{
+		private string roadNo,houseNo, city, country;
+		public Address(string roadNo,string houseNo, string city, string country)
+		{
+			this.roadNo = roadNo;
+			this.houseNo = houseNo;
+			this.city = city;
+			this.country = country;
+		}
+		public string RoadNo
+		{
+			set { this.roadNo = value; }
+			get { return this.roadNo; }
+		}
+		public string HouseNo
+		{
+			set { this.houseNo = value; }
+			get { return this.houseNo; }
+		}
+		public string City
+		{
+			set { this.city = value; }
+			get { return this.city; }
+		}
+		public string Country
+		{
+			set { this.country = value; }
+			get { return this.country; }
+		}
+		public void PrintAddress()
+		{
+			Console.WriteLine("Address:Road No-{0}, House No-{1}, City-{2}, Country-{3}", this.roadNo, this.houseNo, this.city, this.country);
+		}
 	}
 }
--- Assignment111/Program.cs
+++ Assignment111/Program.cs
@@ -10,6 +10,7 @@ namespace Assignment111
 	{
 		static void Main(string[] args)
 		{
+
 		}
 	}
 }
